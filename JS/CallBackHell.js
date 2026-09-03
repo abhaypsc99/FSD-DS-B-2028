fdf02e6cbc5e9c58a404d1737d115b0d5ec155bf
Abhay Pratap Singh <abhayjk123@gmail.com>
2026-09-03 10:08:51 +0530

    callbackhell

---
+++ JS/CallBackHell.js
@@ -0,0 +1,32 @@
+function test1(cb){
+    setTimeout(()=>{
+        console.log("test1")
+        cb();
+    },4000);
+}
+function test2(cb){
+    setTimeout(()=>{
+        console.log("test2")
+        cb();
+    },2000);
+}
+function test3(cb){
+    setTimeout(()=>{
+        console.log("test3")
+        cb();
+    },2000);
+}
+function test4(cb){
+    setTimeout(()=>{
+        console.log("test4");
+        cb();
+    },200);
+}
+
+test1(()=>{
+    test2(()=>{
+        test3(()=>{
+            test4();
+        });
+    });
+});
